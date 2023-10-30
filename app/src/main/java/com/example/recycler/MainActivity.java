c21e128bad87b556b6aae564a804bb84aca03a1a
Durann23 <jd43573@gmail.com>
2023-10-30 13:17:55 -0600

    sipi

--- app/src/main/java/com/example/recycler/MainActivity.java
+++ app/src/main/java/com/example/recycler/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.recycler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i=new Intent(this, Recycler.class);
+
+        new CountDownTimer(5000, 1000)
+        {
+            public void onTick(long msUntilFinished)
+            {
+
+            }
+            public void onFinish() {
+
+                startActivity(i);
+            }
+        }.start();
+
     }
 }
